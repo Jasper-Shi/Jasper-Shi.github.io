ef2158bf68b263595eb7021da589f2c0c035f0d8
Jasper-Shi <31856111+Jasper-Shi@users.noreply.github.com>
2018-03-29 00:00:50 -0400

    fixmap

--- js/custom.js
+++ js/custom.js
@@ -147,7 +147,7 @@ function initialize() {
 
 }
 
-google.maps.event.addDomListener(window, "load", initialize);
+initialize();
 // ========== END GOOGLE MAP ========== //
 
 // ==========  START CONTACT ========== //
