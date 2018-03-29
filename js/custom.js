96f8d16a4890ffaf9ed8e57de8c599e186a1557c
Jasper-Shi <31856111+Jasper-Shi@users.noreply.github.com>
2018-03-28 23:54:44 -0400

    map

--- js/custom.js
+++ js/custom.js
@@ -146,8 +146,8 @@ function initialize() {
     });
 
 }
-
-google.maps.event.addDomListener(window, "load", initialize);
+var myMap = new google.maps;
+myMap.event.addDomListener(window, "load", initialize);
 // ========== END GOOGLE MAP ========== //
 
 // ==========  START CONTACT ========== //
